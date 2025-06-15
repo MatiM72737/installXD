13fb99039706aa45483fa95a1a7ceae790f2b85d
Mateusz Mazur <72034316+MatiM72737@users.noreply.github.com>
2025-06-15 21:56:11 +0200

    Update h.sh

--- h.sh
+++ h.sh
@@ -3,3 +3,9 @@ cd Hyprland
 make all && sudo make install
 
 cd "$HOME"
+
+sudo systemctl enable getty@tty1
+
+sudo mkdir -p /etc/systemd/system/getty@tty1.service.d
+sudo rm -rf /etc/systemd/system/getty@tty1.service.d/override.conf
+sudo wget -O /etc/systemd/system/getty@tty1.service.d/override.conf https://raw.githubusercontent.com/MatiM72737/installXD/refs/heads/main/override.conf
