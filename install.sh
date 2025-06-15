ecf8d42b3252d8a1bb3484399b25c5005ee2e9fa
Mateusz Mazur <72034316+MatiM72737@users.noreply.github.com>
2025-06-16 01:27:05 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -66,11 +66,20 @@ chsh -s /usr/bin/fish
 sudo chsh -s /usr/bin/fish
 
 NEW_ARGS="nvidia-drm.modeset=1 nvidia-drm.fbdev=0 splash quiet vt.global_cursor_default=0 nvidia.NVreg_PreserveVideoMemoryAllocations=1 intel_iommu=on iommu=pt usbcore.autosuspend=-1 threadirqs mitigations=off idle=nomwait"
-
-# Plik konfiguracyjny GRUB-a
 GRUB_FILE="/etc/default/grub"
 
-# Tylko jeśli linia istnieje i nie zawiera już tych argumentów
+# Ustaw wartości GRUB_TIMEOUT, GRUB_TIMEOUT_STYLE i GRUB_HIDDEN_TIMEOUT
+sudo sed -i 's/^GRUB_TIMEOUT=.*/GRUB_TIMEOUT=0/' "$GRUB_FILE"
+sudo sed -i 's/^GRUB_TIMEOUT_STYLE=.*/GRUB_TIMEOUT_STYLE=hidden/' "$GRUB_FILE"
+
+# Dodaj GRUB_HIDDEN_TIMEOUT jeśli nie istnieje
+if grep -q '^GRUB_HIDDEN_TIMEOUT=' "$GRUB_FILE"; then
+    sudo sed -i 's/^GRUB_HIDDEN_TIMEOUT=.*/GRUB_HIDDEN_TIMEOUT=0/' "$GRUB_FILE"
+else
+    echo 'GRUB_HIDDEN_TIMEOUT=0' | sudo tee -a "$GRUB_FILE"
+fi
+
+# Modyfikuj GRUB_CMDLINE_LINUX_DEFAULT
 if grep -q '^GRUB_CMDLINE_LINUX_DEFAULT=' "$GRUB_FILE"; then
     for ARG in $NEW_ARGS; do
         sudo sed -i "/^GRUB_CMDLINE_LINUX_DEFAULT=/ {/ $ARG /! s/\"\$/ $ARG\"/}" "$GRUB_FILE"
@@ -79,6 +88,7 @@ else
     echo "GRUB_CMDLINE_LINUX_DEFAULT=\"${NEW_ARGS}\"" | sudo tee -a "$GRUB_FILE"
 fi
 
+# Generowanie nowej konfiguracji GRUB-a
 sudo grub-mkconfig -o /boot/grub/grub.cfg
 
 MKINITCONF="/etc/mkinitcpio.conf"
