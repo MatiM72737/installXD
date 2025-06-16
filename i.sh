ee51f5eba91b4de19c0eab7ece1dc213671eab66
Mateusz Mazur <72034316+MatiM72737@users.noreply.github.com>
2025-06-16 12:09:52 +0200

    Update i.sh

--- i.sh
+++ i.sh
@@ -20,3 +20,10 @@ sudo /opt/brother/Printers/dcpj105/cupswrapper/cupswrapperdcpj105
 sudo systemctl enable cups
 sudo systemctl start cups
 
+
+sudo rm -rf /etc/default/cpupower
+sudo wget -O /etc/default/cpupower https://raw.githubusercontent.com/MatiM72737/installXD/main/cpupower
+
+
+sudo systemctl enable cpupower.service
+sudo systemctl restart cpupower.service
