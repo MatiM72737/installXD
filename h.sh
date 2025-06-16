c0924be7efb033c3d99a408da11926d6708080b3
Mateusz Mazur <72034316+MatiM72737@users.noreply.github.com>
2025-06-16 12:14:10 +0200

    Update h.sh

--- h.sh
+++ h.sh
@@ -4,6 +4,18 @@ make all && sudo make install
 
 cd "$HOME"
 
+git clone https://github.com/MatiM72737/.myHyprDots.git MyHyprDots
+cd MyHyprDots
+stow --adopt *
+git restore .
+
+wget -O "$HOME/plugins.sh" "https://raw.githubusercontent.com/MatiM72737/installXD/refs/heads/main/plugins.sh"
+chmod +x "$HOME/plugins.sh"
+
+cd "$HOME"
+
+git clone https://github.com/michaelScopic/Wallpapers.git "$HOME/wallpapers"
+cd "$HOME"
 sudo systemctl enable getty@tty1
 
 sudo mkdir -p /etc/systemd/system/getty@tty1.service.d
