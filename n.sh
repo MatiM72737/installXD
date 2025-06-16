cf659fa81750103193ca29c467b88abb7eca4eee
Mateusz Mazur <72034316+MatiM72737@users.noreply.github.com>
2025-06-16 12:17:09 +0200

    Update n.sh

--- n.sh
+++ n.sh
@@ -1,35 +1,18 @@
-cd "$HOME"
-
-git clone https://github.com/MatiM72737/.myHyprDots.git MyHyprDots
-cd MyHyprDots
-stow --adopt *
-git restore .
-
-cd "$HOME"
-
-git clone https://github.com/michaelScopic/Wallpapers.git "$HOME/wallpapers"
-
-sudo /opt/brother/Printers/dcpj105/cupswrapper/cupswrapperdcpj105
-
-sudo systemctl enable cups
-sudo systemctl start cups
-
-sudo rm -rf /etc/default/cpupower
-sudo wget -O /etc/default/cpupower https://raw.githubusercontent.com/MatiM72737/installXD/main/cpupower
-
-
-sudo systemctl enable cpupower.service
-sudo systemctl restart cpupower.service
-
-chsh -s /usr/bin/fish
-sudo chsh -s /usr/bin/fish
-
 NEW_ARGS="nvidia-drm.modeset=1 nvidia-drm.fbdev=0 splash quiet vt.global_cursor_default=0 nvidia.NVreg_PreserveVideoMemoryAllocations=1 intel_iommu=on iommu=pt usbcore.autosuspend=-1 threadirqs mitigations=off idle=nomwait"
-
-# Plik konfiguracyjny GRUB-a
 GRUB_FILE="/etc/default/grub"
 
-# Tylko jeśli linia istnieje i nie zawiera już tych argumentów
+# Ustaw wartości GRUB_TIMEOUT, GRUB_TIMEOUT_STYLE i GRUB_HIDDEN_TIMEOUT
+sudo sed -i 's/^GRUB_TIMEOUT=.*/GRUB_TIMEOUT=0/' "$GRUB_FILE"
+sudo sed -i 's/^GRUB_TIMEOUT_STYLE=.*/GRUB_TIMEOUT_STYLE=hidden/' "$GRUB_FILE"
+
+# Dodaj GRUB_HIDDEN_TIMEOUT jeśli nie istnieje
+if grep -q '^GRUB_HIDDEN_TIMEOUT=' "$GRUB_FILE"; then
+    sudo sed -i 's/^GRUB_HIDDEN_TIMEOUT=.*/GRUB_HIDDEN_TIMEOUT=0/' "$GRUB_FILE"
+else
+    echo 'GRUB_HIDDEN_TIMEOUT=0' | sudo tee -a "$GRUB_FILE"
+fi
+
+# Modyfikuj GRUB_CMDLINE_LINUX_DEFAULT
 if grep -q '^GRUB_CMDLINE_LINUX_DEFAULT=' "$GRUB_FILE"; then
     for ARG in $NEW_ARGS; do
         sudo sed -i "/^GRUB_CMDLINE_LINUX_DEFAULT=/ {/ $ARG /! s/\"\$/ $ARG\"/}" "$GRUB_FILE"
@@ -38,6 +21,7 @@ else
     echo "GRUB_CMDLINE_LINUX_DEFAULT=\"${NEW_ARGS}\"" | sudo tee -a "$GRUB_FILE"
 fi
 
+# Generowanie nowej konfiguracji GRUB-a
 sudo grub-mkconfig -o /boot/grub/grub.cfg
 
 MKINITCONF="/etc/mkinitcpio.conf"
@@ -57,11 +41,3 @@ sudo sed -i -E 's/\<kms\>//g' "$MKINITCONF"
 sudo sed -i -E 's/  +/ /g' "$MKINITCONF"
 
 sudo mkinitcpio -P
-
-LIMITS_FILE="/etc/security/limits.conf"
-RTPRIO_LINE="@audio           -       rtprio          98"
-MEMLOCK_LINE="@audio           -       memlock         unlimited"
-
-# Dodaj tylko jeśli linia jeszcze nie istnieje
-grep -qxF "$RTPRIO_LINE" "$LIMITS_FILE" || echo "$RTPRIO_LINE" | sudo tee -a "$LIMITS_FILE"
-grep -qxF "$MEMLOCK_LINE" "$LIMITS_FILE" || echo "$MEMLOCK_LINE" | sudo tee -a "$LIMITS_FILE"
